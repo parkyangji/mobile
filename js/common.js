644f771c5b4a9e7babaaecfbacff08dbe3332028
parkyangji <dhsfid02@gmail.com>
2023-04-06 14:36:31 +0900

    index

---
+++ js/common.js
@@ -0,0 +1,60 @@
+var op = false; 
+ 	
+$(".gnb_btn").click(function() { 
+    
+   if(op==false){
+     $("#gnb").animate({right:0,opacity:1}, 'fast');
+     $('#headerArea').addClass('mn_open');
+     op=true;
+   }else{
+     $("#gnb").animate({right:'-100%',opacity:0}, 'fast');
+     $('#headerArea').removeClass('mn_open');
+     op=false;
+   }
+
+});
+
+
+$(".depth1 a").click(function(e) {
+  e.preventDefault();
+  $(".depth1").find('ul').hide();
+  $(".depth1 a").css('color', '#333');
+  $(this).css('color', '#1f296f');
+  $(this).parent().next().fadeIn();
+})
+
+
+  
+var smh=$('.mySwiper').height()-$('#headerArea').height();  
+
+ $(window).on('scroll',function(){
+        var scroll = $(window).scrollTop();  
+
+        if(scroll>smh){
+            $('.topMove').fadeIn('slow');
+           
+        }else{
+            $('.topMove').fadeOut('fast');
+        }; 
+        
+});
+
+
+function family_open() {
+  $('.f_family .open').addClass('on');
+  $('.f_family ul').slideDown('200');
+}
+
+function family_close() {
+  $('.f_family .open').removeClass('on');
+  $('.f_family ul').slideUp('200');
+}
+
+$('.f_family .open').toggle(family_open, family_close);
+
+
+$('.topMove').click(function(e){
+        e.preventDefault(); 
+        $("html,body").stop().animate({"scrollTop":0},1000);
+     });
+
